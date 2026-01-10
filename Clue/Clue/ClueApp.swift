1a85a0a28c7b5d21a1fccab005b324890f62841a
AiGames <kashmoney571192@gmail.com>
2026-01-10 12:01:03 -0500

    Add README to gh-pages

---
+++ Clue/Clue/ClueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClueApp.swift
+//  Clue
+//
+//  Created by Aakash Goradia on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ClueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Clue Game
+
+A digital version of the classic Clue detective game.
+
+Live at: https://kashmoney1192.github.io/clue/
